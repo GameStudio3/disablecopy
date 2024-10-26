1465228512821ccb177fc0a59b014439a823ee26
GameStudio3 <89926504+GameStudio3@users.noreply.github.com>
2024-10-26 15:43:11 +0200

    Disable selection using JS

--- disable_copy.js
+++ disable_copy.js
@@ -2,4 +2,6 @@ document.querySelector(".no-copy")
 .addEventListener("copy", e => {
   e.clipboardData.setData("text/plain", "No Copying!");
   e.preventDefault();
-});
+}); // doesn't work against extension (but works without it)
+
+document.querySelector(".no-copy").addEventListener("selectionchange", e => window.getSelection().empty());
